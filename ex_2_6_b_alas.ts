eed25e7ba43dab4559a5092e241c778f472cc738
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-08-30 21:59:05 -0300

    Create ex_2_6_b_alas.ts

---
+++ ex_2_6_b_alas.ts
@@ -0,0 +1,12 @@
+enum DiasDaSemana {
+    Domingo,
+    Segunda,
+    Terça,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sábado
+}
+
+console.log(DiasDaSemana.Domingo); // Saída: 0
+console.log(DiasDaSemana.Sexta); // Saída: 5

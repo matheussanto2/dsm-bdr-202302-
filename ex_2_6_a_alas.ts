f1afc97a22878d892c7e86e5cd06e728cd6dbbd8
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-08-30 21:58:29 -0300

    Create ex_2_6_a_alas.ts

---
+++ ex_2_6_a_alas.ts
@@ -0,0 +1,17 @@
+enum MesesDoAno {
+    Janeiro = 1,
+    Fevereiro,
+    Março,
+    Abril,
+    Maio,
+    Junho,
+    Julho,
+    Agosto,
+    Setembro,
+    Outubro,
+    Novembro,
+    Dezembro
+}
+
+console.log(MesesDoAno.Janeiro); // Saída: 1
+console.log(MesesDoAno.Dezembro); // Saída: 12

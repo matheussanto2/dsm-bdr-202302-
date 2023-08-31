3ea37664a1b44ece21781e35a59036572c328eb8
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-08-30 21:59:27 -0300

    Create ex_2_6_c_alas.ts

---
+++ ex_2_6_c_alas.ts
@@ -0,0 +1,8 @@
+function calcularFatorial(n: number): number {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * calcularFatorial(n - 1);
+}
+
+console.log(calcularFatorial(5)); // Saída: 120 (5! = 5 * 4 * 3 * 2 * 1)

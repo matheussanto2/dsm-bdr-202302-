009b3fabc18244292328223893ee64f1c6ddad8f
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-08-30 22:16:10 -0300

    Create ex_1_1_alas.ts

---
+++ ex_1_1_alas.ts
@@ -0,0 +1,33 @@
+a) Aritmética:
+
+Livro: "Aritmética Elementar" por Gelson Iezzi, Osvaldo Dolce, David Degenszajn e Roberto Périgo
+b) Álgebra elementar:
+
+Livro: "Fundamentos de Matemática Elementar - Álgebra" por Gelson Iezzi, Osvaldo Dolce e Nilze Spirandelli
+c) Álgebra linear:
+
+Livro: "Álgebra Linear e Suas Aplicações" por Gilberto Gilbert Strang
+d) Algoritmos:
+
+Livro: "Algoritmos: Teoria e Prática" por Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest e Clifford Stein (traduzido)
+f) Lógica matemática:
+
+Livro: "Lógica Matemática" por Luiz Kohara, Regina de Almeida e Alexandre Gouvêa
+g) Teoria dos conjuntos:
+
+Livro: "Conjuntos e Números" por Elon Lages Lima
+h) Relações e funções:
+
+Livro: "Matemática Discreta para Computação e Informática" por Edson de Faria, José Carlos de Souza e Jayr Figueiredo de Oliveira
+i) Geometria:
+
+Livro: "Geometria Analítica Moderna" por José Carlos de Souza
+j) Cálculo e análise matemática:
+
+Livro: "Cálculo" por Diva Marília Flemming e Miriam Buss Gonçalves
+k) Probabilidade:
+
+Livro: "Probabilidade e Estatística" por Marcos Nascimento Magalhães e Luis Eduardo da Silva
+l) Estatística:
+
+Livro: "Estatística Básica" por Wilton de Oliveira Bussab e Pedro A. Morettin

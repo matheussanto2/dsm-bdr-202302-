8af789afb64778b8815f8a9bb2767221ecef6a3a
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-08-30 22:08:32 -0300

    Create ex_2_8_a_alas.ts

---
+++ ex_2_8_a_alas.ts
@@ -0,0 +1,7 @@
+def progressao_aritmetica_recursiva(n, a1, r):
+    if n == 1:
+        return a1
+    else:
+        return progressao_aritmetica_recursiva(n - 1, a1, r) + r
+
+print(progressao_aritmetica_recursiva(5, 2, 3))  # Exemplo de chamada recursiva
